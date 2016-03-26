b6a2fe4f8034097bd9c28e86e6ced3bd9fef35a3
Bence Cserna <bence@cserna.net>
2016-03-26 13:11:51 -0400

    FIx logback config

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,7 +1,5 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
-import ch.qos.logback.core.FileAppender
-
 // define the USER_HOME variable setting its value
 // to that of the "user.home" system property
 def USER_HOME = System.getProperty("user.home")
@@ -14,7 +12,7 @@ appender("STDOUT", ConsoleAppender) {
 
 appender("FILE", FileAppender) {
     // make use of the USER_HOME variable
-    println "Setting [file] property to [${USER_HOME}/RTS.log]"
+    println "Setting [file] property to [/home/aifs2/group/data/real_time_search_log/rts_server.log]"
     file =  "/home/aifs2/group/data/real_time_search_log/rts_server.log"
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} %5level %logger{0} - %msg%n"
